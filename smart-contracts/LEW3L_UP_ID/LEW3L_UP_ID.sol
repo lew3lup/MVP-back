9c440503499242412a402506ccbd4bb5b0f1a96e
Sergiy <sergey-sarkazm@yandex.ru>
2023-03-04 11:38:21 +0300

    NFT-ID initial

---
+++ smart-contracts/LEW3L_UP_ID/LEW3L_UP_ID.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./Ownable.sol";
+import "./ISBT721.sol";
+import "./IERC721Metadata.sol";
+
+contract LEW3L_UP_ID is Ownable, ISBT721, IERC721Metadata {
+
+}
