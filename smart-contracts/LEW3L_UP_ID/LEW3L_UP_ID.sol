92d229c02d6f7ca9cfbc91fbb15cfb7fa5e55d03
Sergiy <sergey-sarkazm@yandex.ru>
2023-03-07 16:06:51 +0300

    NFT ID in progress

--- smart-contracts/LEW3L_UP_ID/LEW3L_UP_ID.sol
+++ smart-contracts/LEW3L_UP_ID/LEW3L_UP_ID.sol
@@ -6,5 +6,40 @@ import "./ISBT721.sol";
 import "./IERC721Metadata.sol";
 
 contract LEW3L_UP_ID is Ownable, ISBT721, IERC721Metadata {
+    
+    function _verifySignature(uint _id, uint16 _country, uint32 _birthDate, address _signer, bytes memory _sign) internal view returns (bool) {
+        bytes32 hash = keccak256(abi.encodePacked(
+                "\x19Ethereum Signed Message:\n32",
+                keccak256(abi.encode(chainId, _id, _country, _birthDate))
+            ));
+        address[] memory signList = _recoverAddresses(hash, _sign);
+        return signList[0] == _signer;
+    }
 
+    function _recoverAddresses(bytes32 _hash, bytes memory _signatures) pure internal returns (address[] memory addresses) {
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+        uint count = _countSignatures(_signatures);
+        addresses = new address[](count);
+        for (uint i = 0; i < count; i++) {
+            (v, r, s) = _parseSignature(_signatures, i);
+            addresses[i] = ecrecover(_hash, v, r, s);
+        }
+    }
+
+    function _parseSignature(bytes memory _signatures, uint _pos) pure internal returns (uint8 v, bytes32 r, bytes32 s) {
+        uint offset = _pos * 65;
+        assembly {
+            r := mload(add(_signatures, add(32, offset)))
+            s := mload(add(_signatures, add(64, offset)))
+            v := and(mload(add(_signatures, add(65, offset))), 0xff)
+        }
+        if (v < 27) v += 27;
+        require(v == 27 || v == 28);
+    }
+
+    function _countSignatures(bytes memory _signatures) pure internal returns (uint) {
+        return _signatures.length % 65 == 0 ? _signatures.length / 65 : 0;
+    }
 }
