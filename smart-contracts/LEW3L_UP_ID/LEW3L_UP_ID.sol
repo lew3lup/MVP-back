fea0268e030da1ffdd3c9596923d8d8eec089182
Sergiy <sergey-sarkazm@yandex.ru>
2023-03-10 12:12:00 +0300

    LEW3L-UP ID smart contract in progress

--- smart-contracts/LEW3L_UP_ID/LEW3L_UP_ID.sol
+++ smart-contracts/LEW3L_UP_ID/LEW3L_UP_ID.sol
@@ -2,13 +2,15 @@
 pragma solidity ^0.8.0;
 
 import "./Ownable.sol";
+import "./IERC721.sol";
 import "./ISBT721.sol";
 import "./IERC721Metadata.sol";
 import "./EnumerableMap.sol";
 import "./Counters.sol";
 import "./Strings.sol";
+import "./ERC165.sol";
 
-contract LEW3L_UP_ID is Ownable, ISBT721, IERC721Metadata {
+contract LEW3L_UP_ID is Ownable, ERC165, ISBT721, IERC721Metadata {
     using Strings for uint256;
     using Counters for Counters.Counter;
     using EnumerableMap for EnumerableMap.AddressToUintMap;
@@ -30,8 +32,11 @@ contract LEW3L_UP_ID is Ownable, ISBT721, IERC721Metadata {
     // Token URI
     string private _baseTokenURI;
 
+    //ToDo: переназначение этих параметров
     address private _signer;
     address private _attester;
+
+    //ToDo: конструктор
 
     function attest(address to) external returns (uint256) {
         require(_msgSender() == _attester, "Forbidden");
@@ -82,20 +87,20 @@ contract LEW3L_UP_ID is Ownable, ISBT721, IERC721Metadata {
     }
 
     function mint(bytes memory sign) external returns (uint256) {
+        address to = _msgSender();
+        require(!_tokenMap.contains(to), "Token already exists");
+        require(_verifySignature(to, sign), "Incorrect signature");
 
-    }
+        _tokenId.increment();
+        uint256 tokenId = _tokenId.current();
 
-    function totalSupply() external view returns (uint256) {
-        return _tokenMap.length();
-    }
+        _tokenMap.set(to, tokenId);
+        _ownerMap.set(tokenId, to);
 
-    /**
-     * @dev See {IERC721Metadata-tokenURI}.
-     */
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        require(_exists(tokenId), "URI query for nonexistent token");
-        string memory baseURI = verifiers[passports[tokenId].verifierId].baseUri;
-        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
+        emit Attest(to, tokenId);
+        emit Transfer(address(0), to, tokenId);
+
+        return tokenId;
     }
 
     /**
@@ -126,8 +131,7 @@ contract LEW3L_UP_ID is Ownable, ISBT721, IERC721Metadata {
      * @dev See {IERC721Metadata-tokenURI}.
      */
     function tokenURI(uint256 tokenId) external view returns (string memory) {
-        return
-        bytes(_baseTokenURI).length > 0
+        return bytes(_baseTokenURI).length > 0
         ? string(abi.encodePacked(_baseTokenURI, tokenId.toString()))
         : "";
     }
@@ -139,20 +143,17 @@ contract LEW3L_UP_ID is Ownable, ISBT721, IERC721Metadata {
         return interfaceId == type(IERC721).interfaceId || interfaceId == type(IERC721Metadata).interfaceId || super.supportsInterface(interfaceId);
     }
 
-    function _verifySignature(address tokenOwner, address signer, bytes memory sign) internal view returns (bool) {
-        bytes32 hash = keccak256(abi.encodePacked(
-                "\x19Ethereum Signed Message:\n32",
-                keccak256(tokenOwner)
-            ));
+    function _verifySignature(address to, bytes memory sign) internal view returns (bool) {
+        bytes32 hash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", to));
         address[] memory signList = _recoverAddresses(hash, sign);
-        return signList[0] == signer;
+        return signList[0] == _signer;
     }
 
     function _recoverAddresses(bytes32 hash, bytes memory signatures) pure internal returns (address[] memory addresses) {
         uint8 v;
         bytes32 r;
         bytes32 s;
-        uint count = _countSignatures(_signatures);
+        uint count = _countSignatures(signatures);
         addresses = new address[](count);
         for (uint i = 0; i < count; i++) {
             (v, r, s) = _parseSignature(signatures, i);
