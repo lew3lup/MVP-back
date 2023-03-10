542d93bb5e6dc58fca92acecd3a98d0a6efb8b20
Sergiy <sergey-sarkazm@yandex.ru>
2023-03-10 11:12:41 +0300

    LEW3L-UP ID smart contract in progress

--- smart-contracts/LEW3L_UP_ID/LEW3L_UP_ID.sol
+++ smart-contracts/LEW3L_UP_ID/LEW3L_UP_ID.sol
@@ -4,42 +4,174 @@ pragma solidity ^0.8.0;
 import "./Ownable.sol";
 import "./ISBT721.sol";
 import "./IERC721Metadata.sol";
+import "./EnumerableMap.sol";
+import "./Counters.sol";
+import "./Strings.sol";
 
 contract LEW3L_UP_ID is Ownable, ISBT721, IERC721Metadata {
-    
-    function _verifySignature(uint _id, uint16 _country, uint32 _birthDate, address _signer, bytes memory _sign) internal view returns (bool) {
-        bytes32 hash = keccak256(abi.encodePacked(
-                "\x19Ethereum Signed Message:\n32",
-                keccak256(abi.encode(chainId, _id, _country, _birthDate))
-            ));
-        address[] memory signList = _recoverAddresses(hash, _sign);
-        return signList[0] == _signer;
+    using Strings for uint256;
+    using Counters for Counters.Counter;
+    using EnumerableMap for EnumerableMap.AddressToUintMap;
+    using EnumerableMap for EnumerableMap.UintToAddressMap;
+
+    // Mapping from token ID to owner address
+    EnumerableMap.UintToAddressMap private _ownerMap;
+    EnumerableMap.AddressToUintMap private _tokenMap;
+
+    // Token Id
+    Counters.Counter private _tokenId;
+
+    // Token name
+    string public name;
+
+    // Token symbol
+    string public symbol;
+
+    // Token URI
+    string private _baseTokenURI;
+
+    address private _signer;
+    address private _attester;
+
+    function attest(address to) external returns (uint256) {
+        require(_msgSender() == _attester, "Forbidden");
+        require(to != address(0), "Address is empty");
+        require(!_tokenMap.contains(to), "Token already exists");
+
+        _tokenId.increment();
+        uint256 tokenId = _tokenId.current();
+
+        _tokenMap.set(to, tokenId);
+        _ownerMap.set(tokenId, to);
+
+        emit Attest(to, tokenId);
+        emit Transfer(address(0), to, tokenId);
+
+        return tokenId;
     }
 
-    function _recoverAddresses(bytes32 _hash, bytes memory _signatures) pure internal returns (address[] memory addresses) {
+    function revoke(address from) external {
+        require(_msgSender() == _attester, "Forbidden");
+        require(from != address(0), "Address is empty");
+        require(_tokenMap.contains(from), "The account does not have any token");
+
+        uint256 tokenId = _tokenMap.get(from);
+
+        _tokenMap.remove(from);
+        _ownerMap.remove(tokenId);
+
+        emit Revoke(from, tokenId);
+        emit Transfer(from, address(0), tokenId);
+    }
+
+    function burn() external {
+        address sender = _msgSender();
+
+        require(
+            _tokenMap.contains(sender),
+            "The account does not have any token"
+        );
+
+        uint256 tokenId = _tokenMap.get(sender);
+
+        _tokenMap.remove(sender);
+        _ownerMap.remove(tokenId);
+
+        emit Burn(sender, tokenId);
+        emit Transfer(sender, address(0), tokenId);
+    }
+
+    function mint(bytes memory sign) external returns (uint256) {
+
+    }
+
+    function totalSupply() external view returns (uint256) {
+        return _tokenMap.length();
+    }
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "URI query for nonexistent token");
+        string memory baseURI = verifiers[passports[tokenId].verifierId].baseUri;
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
+    }
+
+    /**
+     * @dev Update _baseTokenURI
+     */
+    function setBaseTokenURI(string calldata uri) public onlyOwner {
+        _baseTokenURI = uri;
+    }
+
+    function balanceOf(address owner) external view returns (uint256) {
+        (bool success, ) = _tokenMap.tryGet(owner);
+        return success ? 1 : 0;
+    }
+
+    function tokenIdOf(address from) external view returns (uint256) {
+        return _tokenMap.get(from, "The wallet has not attested any SBT");
+    }
+
+    function ownerOf(uint256 tokenId) external view returns (address) {
+        return _ownerMap.get(tokenId, "Invalid tokenId");
+    }
+
+    function totalSupply() external view returns (uint256) {
+        return _tokenMap.length();
+    }
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId) external view returns (string memory) {
+        return
+        bytes(_baseTokenURI).length > 0
+        ? string(abi.encodePacked(_baseTokenURI, tokenId.toString()))
+        : "";
+    }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return interfaceId == type(IERC721).interfaceId || interfaceId == type(IERC721Metadata).interfaceId || super.supportsInterface(interfaceId);
+    }
+
+    function _verifySignature(address tokenOwner, address signer, bytes memory sign) internal view returns (bool) {
+        bytes32 hash = keccak256(abi.encodePacked(
+                "\x19Ethereum Signed Message:\n32",
+                keccak256(tokenOwner)
+            ));
+        address[] memory signList = _recoverAddresses(hash, sign);
+        return signList[0] == signer;
+    }
+
+    function _recoverAddresses(bytes32 hash, bytes memory signatures) pure internal returns (address[] memory addresses) {
         uint8 v;
         bytes32 r;
         bytes32 s;
         uint count = _countSignatures(_signatures);
         addresses = new address[](count);
         for (uint i = 0; i < count; i++) {
-            (v, r, s) = _parseSignature(_signatures, i);
-            addresses[i] = ecrecover(_hash, v, r, s);
+            (v, r, s) = _parseSignature(signatures, i);
+            addresses[i] = ecrecover(hash, v, r, s);
         }
     }
 
-    function _parseSignature(bytes memory _signatures, uint _pos) pure internal returns (uint8 v, bytes32 r, bytes32 s) {
-        uint offset = _pos * 65;
+    function _parseSignature(bytes memory signatures, uint pos) pure internal returns (uint8 v, bytes32 r, bytes32 s) {
+        uint offset = pos * 65;
         assembly {
-            r := mload(add(_signatures, add(32, offset)))
-            s := mload(add(_signatures, add(64, offset)))
-            v := and(mload(add(_signatures, add(65, offset))), 0xff)
+            r := mload(add(signatures, add(32, offset)))
+            s := mload(add(signatures, add(64, offset)))
+            v := and(mload(add(signatures, add(65, offset))), 0xff)
         }
         if (v < 27) v += 27;
         require(v == 27 || v == 28);
     }
 
-    function _countSignatures(bytes memory _signatures) pure internal returns (uint) {
-        return _signatures.length % 65 == 0 ? _signatures.length / 65 : 0;
+    function _countSignatures(bytes memory signatures) pure internal returns (uint) {
+        return signatures.length % 65 == 0 ? signatures.length / 65 : 0;
     }
 }
